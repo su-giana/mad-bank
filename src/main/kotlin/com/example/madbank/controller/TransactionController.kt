cb031892ab2b88021986525ef6a42a9ccd1674b3
su-giana <giananews@gmail.com>
2023-07-15 20:56:42 +0900

    15 Jul. (Sujee) : Controller bug resolve try #2

--- src/main/kotlin/com/example/madbank/controller/TransactionController.kt
+++ src/main/kotlin/com/example/madbank/controller/TransactionController.kt
@@ -1,5 +1,6 @@
 package com.example.madbank.controller
 
+import com.example.madbank.model.TransferForm
 import com.example.madbank.model.User
 import com.example.madbank.security.JwtTokenUtil
 import com.example.madbank.service.TransactionService
@@ -9,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class TransactionController {
@@ -25,11 +28,7 @@ class TransactionController {
 
     @GetMapping("/transfer_money")
     public fun transferMoney(
-            @RequestParam(value="transactionId", required=true) transactionId:Long,
-            @RequestParam(value="senderId", required = true) senderId:Long,
-            @RequestParam(value="receiverId", required = true) receiverId:Long,
-            @RequestParam(value="transactionType", required = true) transactionType:String,
-            @RequestParam(value="cost", required = true) cost:Long,
+            @RequestBody transferForm: TransferForm
     ): ResponseEntity<String>
     {
         //1. senderId와 receiverId가 모두 DB에 존재하는지 확인한다. => userService.isUserAlreadyExist
@@ -37,14 +36,20 @@ class TransactionController {
         //3, senderId의 금액 차감 =>deductSenderBalance, UpdateUser
         //4. receiverId의 금액 추가 =>addReceiverBalance, UpdateUser
         //5. resultCode를 'Success'로 설정한다.
+
+        val transactionType:Long = transferForm.transactionType
+        val senderId:Long = transferForm.senderId
+        val receiverId:Long = transferForm.receiverId
+        val cost = transferForm.cost
+
         try {
 
             if(userService.isUserAlreadyExist(senderId)&&userService.isUserAlreadyExist(receiverId)){
                 if(transactionService.isBalanceEnough(senderId, cost)){
                     transactionService.deductSenderBalance(senderId, cost)
                     transactionService.addReceiverBalance(receiverId, cost)
-                    transactionService.admitTransfercode(transactionId)
-                    return ResponseEntity.ok("$transactionId transfer success. happyhappyhappy")
+                    transactionService.admitTransfercode(transactionType)
+                    return ResponseEntity.ok("$transactionType transfer success. happyhappyhappy")
                 }else{
                     return ResponseEntity.ok("Error Occur: Sender doesn't have enough money to transfer haha.\uD83D\uDC7B from JY")
                 }
---
+++ src/main/kotlin/com/example/madbank/model/TransferForm.kt
@@ -0,0 +1,14 @@
+package com.example.madbank.model
+
+import lombok.AllArgsConstructor
+import lombok.Data
+import org.springframework.web.bind.annotation.RequestParam
+
+@Data
+@AllArgsConstructor
+data class TransferForm (
+        val transactionType:Long,
+        val senderId:Long,
+        val receiverId:Long,
+        val cost:Long
+)
