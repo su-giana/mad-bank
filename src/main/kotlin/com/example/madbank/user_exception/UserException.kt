ea9140577ab0c256ffd433b2238b785825e1b494
su-giana <giananews@gmail.com>
2023-07-15 16:09:39 +0900

    15 Jul. (Sujee) : account logic issue resolving..

--- src/main/kotlin/com/example/madbank/user_exception/UserException.kt
+++ src/main/kotlin/com/example/madbank/user_exception/UserException.kt
@@ -5,13 +5,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
-class NotExistingUserException(message : String) : Exception(message) {
-}
+class NotExistingUserException(message : String) : Exception(message) {}
 
-class PasswordNotMatchesException(message : String) : Exception(message)
-{
-
-}
+class PasswordNotMatchesException(message : String) : Exception(message) {}
 
 class NotValidTokenException(message:String):Exception(message){}
 
@@ -22,21 +18,25 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(NotExistingUserException::class)
     fun handleNotExistingUserException(ex: NotExistingUserException): ResponseEntity<String> {
+        ex.printStackTrace()
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 
     @ExceptionHandler(PasswordNotMatchesException::class)
     fun handlePasswordNotMatchesException(ex: PasswordNotMatchesException): ResponseEntity<String> {
+        ex.printStackTrace()
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 
     @ExceptionHandler(NotValidTokenException::class)
     fun handleNotValidTokenException(ex: NotValidTokenException): ResponseEntity<String> {
+        ex.printStackTrace()
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 
     @ExceptionHandler(AlreadyRegisteredException::class)
     fun handleAlreadyRegisteredException(ex: AlreadyRegisteredException): ResponseEntity<String> {
+        ex.printStackTrace()
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 
